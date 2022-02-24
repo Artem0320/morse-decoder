f07f21d813ec535cf1c535fbbcdc01ecd8db698c
Artem Gorbachev <Ag2003g@yandex.ru>
2022-02-25 00:19:30 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = []
+    const morse = []
+    let result = ''
+    for(let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        if(!arr[i].includes('*')) {
+            morse[i] = ''
+            for(let n = 0; n < 10; n += 2) {
+                if(arr[i].slice(n, n + 2) == 10) {
+                    morse[i] += '.'
+                } else if(arr[i].slice(n, n + 2) == 11) {
+                    morse[i] += '-'
+                }
+            }
+        } else {
+            morse[i] = 'sp'
+        }
+        
+    }
+
+    for(let el of morse) {
+        if(el == 'sp') {
+            result += ' '
+        } else {
+            result += MORSE_TABLE[el]
+        }
+    }
+
+    return result
 }
 
 module.exports = {
